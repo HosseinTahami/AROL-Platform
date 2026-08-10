ec256560fa56594efaa95096e831d44bfc4fcde9
Hossein Tahami <hosseintahami2000@gmail.com>
2026-08-10 09:40:14 +0200

    Add: admin registration

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Company, User
+
+admin.site.register(Company)
+admin.site.register(User, UserAdmin)
