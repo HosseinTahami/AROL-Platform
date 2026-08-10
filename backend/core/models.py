de0805443cbb32bbea21dcbca29a8e22f806d53e
Hossein Tahami <hosseintahami2000@gmail.com>
2026-08-10 09:08:00 +0200

    Add: Company model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+
+    company_id = models.CharField(max_length=64, primary_key=True)
+    company_name = models.CharField(max_length=128)
+    country = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    sector = models.CharField(max_length=128)
+    currency = models.CharField(max_length=8)
+    locale = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+
+    def __str__(self):
+        return self.company_name
