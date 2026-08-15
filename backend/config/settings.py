ab7b39afdaa80dee7d20da1912f3cbf4bf40e5ca
Hossein Tahami <hosseintahami2000@gmail.com>
2026-08-15 10:01:26 +0200

    Chore: CORS for django

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     "core",
     "rest_framework",
     "rest_framework.authtoken",
-    "ai"
+    "ai",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,3 +159,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
+
+
+# CORS
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
